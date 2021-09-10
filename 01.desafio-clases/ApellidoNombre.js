ee7946036a72582943da9a249a974cb05e9fe9b7
Gonzalo <gmsanchezgaray@gmail.com>
2021-09-09 22:56:47 -0300

    desafio 01-clases

---
+++ 01.desafio-clases/ApellidoNombre.js
@@ -0,0 +1,44 @@
+class Usuario {
+  constructor(nombre, apellido, libros, mascotas) {
+    this.nombre = nombre; //String
+    this.apellido = apellido; //String
+    this.libros = libros; //Object[]
+    this.mascotas = mascotas; //String[]
+  }
+  getFullName() {
+    console.log(`${this.nombre} ${this.apellido}`);
+  }
+
+  addBook(nombreLibro, autorLibro) {
+    this.libros.push({ nombre: nombreLibro, autor: autorLibro });
+  }
+
+  getBookNames() {
+    const arrayNombreLibros = this.libros.map((libro) => libro.nombre);
+    console.log(arrayNombreLibros);
+  }
+
+  addMascota(nombreMascota) {
+    this.mascotas.push(nombreMascota);
+  }
+  countMascotas() {
+    console.log(this.mascotas.length, "mascotas");
+  }
+}
+
+//Nueva instancia de prueba
+const usuario1 = new Usuario(
+  "Elon",
+  "Musk",
+  [
+    { nombre: "El señor de las moscas", autor: "William Golding" },
+    { nombre: "Fundacion", autor: "Isaac Asimov" },
+  ],
+  ["Firulais", "Pulguita"]
+);
+
+usuario1.getFullName();
+usuario1.countMascotas();
+usuario1.addMascota("Scooby");
+usuario1.countMascotas();
+usuario1.getBookNames();
